1b7c12cc75cd07eb307ad6825b0928cbb2725af8
namper <nnamper@gmail.com>
2020-06-14 01:18:32 +0400

    Activity Greedy Selector

---
+++ AS/ActivitySelection.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+void printSolution(vector <int> A)
+{   
+    cout<<"ამორჩეული განცხადებების ნომერებია:  ";
+    vector <int> :: iterator it; 
+    for(it = A.begin(); it != A.end(); ++it) 
+        cout <<*it<<" "; 
+}
+
+void GreedySelector(vector<int> s, vector<int> f) 
+{ 
+  if(s.size() == f.size()){
+      int i = 0;
+      int n = s.size();
+      vector <int> A;
+      A.push_back(i);
+
+    for (int j = 1; j < n; j++) 
+    { 
+          if (s[j] >= f[i]) 
+          { 
+              A.push_back(j);
+              i = j; 
+          } 
+    } 
+      printSolution(A);
+  }else{
+    cout << "არასოწრი მონაცემები" << endl;
+  }
+} 
+
+int main() 
+{   
+	vector<int> s = {1, 3, 2, 5, 8, 5, 10}; 
+	vector<int> f = {3, 7, 8, 9, 10, 12, 14}; 
+	GreedySelector(s, f); 
+	return 0; 
+} 
